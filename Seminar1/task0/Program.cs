dc4b5915939a1d78138750ad913de5b332b31a22
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-11 16:12:11 -0300

    Greetings, new commands,tasks

---
+++ Seminar1/task0/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Начало работы:
+// 1) создаем новую папку
+// 2) открываем в СООТВЕТСТВУЮЩЕМ ТЕРМИНАЛЕ или через команду cd 
+// 3)инициируем документ командой: dotnet new console
+// 4) убеждаемся, что работает: dotnet run
+
+// //Освоение простых команд:   
+// 1) Закомментировать абзац command + /
+
+// Console.Write(); вывод в одну строку
+// Console.WriteLine(); вывод и переход на новую строку
+// Console.ReadLine(); считать строку из терминала 
+
+// Приветствие:
+// Console.WriteLine("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+
+// Сложение двух чисел:
+//  1) определяем перемнную и кладем в нее какое-значение, например 3:
+//  numberA = 3;
+//  2) говорим компьютеру, сколько памяти надо выделить и определяем тип числа, например целое число:
+//  int numberB = 5;
+//  3) запускаем алгоритм сложения 1:
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+//  3) запускаем алгоритм сложения 2:
+// int numberA = 3;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+
+
+
+
+ 
+
+
+
+
+// Возведение в степень:
+// Console.WriteLine("Введите число");
+// string answer = Console.ReadLine();
+// int number = Convert.ToInt32(answer);
+// Console.WriteLine(number*number);
+
+
+ 
+
+
+
+
+
+
