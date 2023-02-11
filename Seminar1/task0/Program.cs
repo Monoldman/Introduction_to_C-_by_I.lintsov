b73769bc70def63c05e05489137c860251b9282d
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-11 17:01:51 -0300

    Added division

--- Seminar1/task0/Program.cs
+++ Seminar1/task0/Program.cs
@@ -35,13 +35,19 @@
 // int result = numberA + numberB;
 // Console.WriteLine(result);
 
+// Делените двух цлых чисел:
+//  1) определяем перемннуые и кладем в них значения:
+// int numberA = 12;
+// int numberB = 5;
+//  2) запускаем алгоритм деления:
+// int numberA = 12;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
 
-
-
-
- 
-
-
+// Делените двух вещественных чисел:
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
 
 
 // Возведение в степень:
