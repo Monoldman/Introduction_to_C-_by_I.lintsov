c713f2e9488d0cde0007bf954e0b75edba37342d
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-13 22:32:17 -0300

    Task finding max

--- Seminar1/task0/Program.cs
+++ Seminar1/task0/Program.cs
@@ -89,7 +89,24 @@
 
 // Для решения следующей задачи создаю новую докумпент: task1
 
+// Решение задачи с гирями, которые нужно сравнить. 
 
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if  (a > max) max = a;
+if  (b > max) max = b;
+if  (c > max) max = c;
+if  (d > max) max = d;
+if  (e > max) max = e;
+
+Console.Write("max = "); 
+Console.WriteLine(max);
 
 
 
