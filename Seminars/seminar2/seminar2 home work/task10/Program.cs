307550e91004686800432ef5c0a23539cb05d3a9
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-17 20:19:46 -0300

    Finished working  on Seminar2 clear

---
+++ Seminars/seminar2/seminar2 home work/task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// HOME WORK (Seminar2)
+
+// Задача 10 смеминар 2 условие:
+// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Например:
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+
+Console.WriteLine("Введите техзначное число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 100 || userNumber > 999 ) Console.WriteLine("Введите именно техзначное число");
+
+else {Console.WriteLine((userNumber%100)/10);}
