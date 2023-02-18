dc2b4d1550f4c54688e260b5bf56f3afccf31589
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-17 22:02:52 -0300

    Added folder Theory

---
+++ Seminars/Theory/for/Program.cs
@@ -0,0 +1,32 @@
+﻿// цикл                   FOR 
+
+for (int i = 0; случайное число > определенное задаваемое число ; i++) {случайное число = случайное число и действие с ним;}
+
+
+
+
+
+
+// HOME WORK (Seminar2)
+
+// Задача 13 смеминар 2 условие:
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+
+// Решение задачи через цикл                   FOR 
+
+
+// int RandomUserNumber = Random.Shared.Next(0, int.MaxValue);
+// Console.WriteLine(RandomUserNumber);
+
+// if (RandomUserNumber < 100) Console.WriteLine(RandomUserNumber + " не хватает третьей цифры");
+
+// for (; RandomUserNumber > 999;) {RandomUserNumber = RandomUserNumber / 10;}
+
+// Console.WriteLine(RandomUserNumber % 10);
