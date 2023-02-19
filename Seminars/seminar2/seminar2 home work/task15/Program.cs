a920fdb1d5634150a757886ff88ce01597359fc0
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-19 00:39:29 -0300

    Added task15

---
+++ Seminars/seminar2/seminar2 home work/task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// HOME WORK (Seminar2)
+
+// Задача 15 смеминар 2 условие:
+// Задача 15: Напишите программу, которая 
+// принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 1 || userNumber > 7) Console.WriteLine("Введите цифру, обозначающую день недели");
+
+else if (userNumber == 6 || userNumber == 7) Console.WriteLine("Это выходной день");
+else {Console.WriteLine("Это рабочий день");}
+
+
+
+
