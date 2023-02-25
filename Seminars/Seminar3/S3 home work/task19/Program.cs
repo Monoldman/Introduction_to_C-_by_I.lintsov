83c7f7ae37aca22a23fb39d8079b074903e61cb4
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-25 16:38:26 -0300

    Added task19 to S3 home work

--- Seminars/Seminar3/S3 home work/task19/Program.cs
+++ Seminars/Seminar3/S3 home work/task19/Program.cs
@@ -2,3 +2,13 @@
 // Напишите программу, которая принимает на вход 
 // пятизначное число и проверяет, является ли оно палиндромом.
 
+Console.WriteLine("Введите пятизначное число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 10000 || userNumber > 99999 ) Console.WriteLine("Введите именно пятизначное число");
+
+else if (userNumber/10000 == userNumber % 10 && userNumber/1000%10 == userNumber / 10 % 10) Console.WriteLine("Да, это число палиндром");
+
+else {Console.WriteLine("Это число не палиндром");}
+
+
