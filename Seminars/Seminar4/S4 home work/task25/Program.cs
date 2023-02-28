720d3ef1e82dc913a41584c0e9659d1b4ff2fc3f
Denis Uvarov <denisuvarov@192.168.1.2>
2023-02-28 00:34:57 -0300

    Added task 25 to S4 home work

---
+++ Seminars/Seminar4/S4 home work/task25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+
+int ReadNumber(string message) 
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value; 
+}
+
+int userNumberA(int a)
+int userNumberB(int b)
+
+{
+    int count = 0;
+    for (int i = a; i <= b; i++)
+    {
+        count = Math.Pow(a, b); //sum += i;  
+    }
+
+    return count;
+}
+
+int number = userNumberA("Введите число А");
+
+ int result = count(number);
+ Console.WriteLine(result);
